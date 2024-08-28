7585e72de29e51aa0ccdec89fa1988f9fa7f8a09
masaki <m.shimanuki614@gmail.com>
2024-08-28 09:46:08 +0900

    move site add

--- move_site/urls.py
+++ move_site/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = "move_site"
 urlpatterns = [
     path("", views.movesite, name="index"),
+    path("scroll/", views.scroll, name="scroll"),
+    path("scroll_animation/", views.scroll_animation, name="scroll_animation"),
+    path("bootstrap/", views.bootstrap, name="bootstrap"),
 ]
--- move_site/views.py
+++ move_site/views.py
@@ -9,4 +9,25 @@ class MoveSiteView(View):
         info: dict = getattr(settings, "INFO", None)
         return render(request, "move_site/index.html", {"info": info})       
 
+class ScrollView(View):
+    def get(self, request):
+        # info.ymlの辞書型データを取得
+        info: dict = getattr(settings, "INFO", None)
+        return render(request, "move_site/scroll.html", {"info": info})       
+
+class ScrollAnimationView(View):
+    def get(self, request):
+        # info.ymlの辞書型データを取得
+        info: dict = getattr(settings, "INFO", None)
+        return render(request, "move_site/scroll_animation.html", {"info": info})       
+
+class BootstrapView(View):
+    def get(self, request):
+        # info.ymlの辞書型データを取得
+        info: dict = getattr(settings, "INFO", None)
+        return render(request, "move_site/bootstrap.html", {"info": info})       
+
 movesite = MoveSiteView.as_view()
+scroll = ScrollView.as_view()
+scroll_animation = ScrollAnimationView.as_view()
+bootstrap = BootstrapView.as_view()
